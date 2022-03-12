01f4a081094ace6a9fcb155b41ce150f98ea0309
Chang Yi Chen <changyichen@u.nus.edu>
2022-03-12 15:19:53 +0800

    Sprint 1: Add in folders for components, views and router

--- my-team/src/App.vue
+++ my-team/src/App.vue
@@ -1,10 +1,14 @@
 <template>
-<h1>My-Team</h1>
+    <h1>My-Team</h1>
+    <div id="nav">
+        <router-link to = "/">Table</router-link>
+    </div>
+    <router-view/>
 </template>
 
 <script>
 export default {
-
+    name: 'App'
 }
 </script>
 
---
+++ my-team/src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import ProjectPage from '@/views/ProjectPage.vue'
+
+const routes = [
+    {
+        path: '/',
+        name: 'ProjectPage',
+        component: ProjectPage
+    }
+]
+
+const router = createRouter({
+ history: createWebHistory(),
+ routes
+})
+export default router
