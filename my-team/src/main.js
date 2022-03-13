71c7126888a640810e2ff4772574bec14fd77557
eryuanren <eryuanren@gmail.com>
2022-03-13 21:44:13 +0800

    Created a sidebar menu for navigation
    Still require the buttons for the different views and logout

--- my-team/src/main.js
+++ my-team/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router/index.js'
 import naive from 'naive-ui'
 
+import '@fortawesome/fontawesome-free/js/all'
+
 const app = createApp(App)
 app.use(naive)
 
