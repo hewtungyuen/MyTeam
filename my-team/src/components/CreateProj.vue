b1dd9b2b953951109d4597a9c59c2dba17cfcc12
ziyuanqin <e0563845@u.nus.edu>
2022-03-14 21:57:26 +0800

    front end for create proj and router to this page

--- my-team/src/components/CreateProj.vue
+++ my-team/src/components/CreateProj.vue
@@ -1,5 +1,26 @@
 <template>
-  
+  <div id = "body">
+    <h1 id = "heading"> Create a new project </h1>
+    <div id = "proj_name">
+        <form>
+        <label for="name" >Project Name: </label> <br><br>
+        <input type="text" id="name" required="" size = "50"> <br><br><br>
+        <label for="details">Project Details: </label> <br><br>
+        <textarea id="details" name="details" rows="4" cols="50"></textarea>
+        </form>
+    </div>
+    <div id = "add_member">
+        <form>
+            <label for="add" >Add New Members: </label> <br><br>
+            <input type="email" id="add" required="" size = "40">
+            <button id = 'add_new' round>
+                +
+            </button> <br><br><br>
+            <label for="members">Team Members: </label> <br><br>
+            <textarea id="members" name="members" rows="4" cols="50"></textarea>
+        </form>
+    </div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +30,35 @@ export default {
 </script>
 
 <style>
+#body{
+            position:fixed;
+            margin: 35px;
+            font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        }
+        #heading{
+        margin-left: 550px
+        /* Add a new project */
+        }
+        #proj_name{
+            margin-top: 50px;
+            margin-left: 60px;
+            display:inline-block;
+        }
+        #add_member{
+            margin-top:50px;
+            margin-left: 60px;
+            display: inline-block;
+            
+        }
+        form{
+            font-size: 24px;
+
+        }
+        input, textarea{
+            border: 1px solid black;
+            font-size: 20px;
+            font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        }
+
 
 </style>
--- my-team/src/router/index.js
+++ my-team/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ProjectPage from '@/views/ProjectPage.vue'
 import HomePage from '@/views/HomePage.vue'
+import NewProjPage from '@/views/NewProjPage.vue'
 
 const routes = [
     {
@@ -13,6 +14,12 @@ const routes = [
         path: '/HomePage',
         name:'HomePage',
         component: HomePage
+    },
+    
+    {
+        path: '/NewProjPage',
+        name: 'NewProjPage',
+        component: NewProjPage
     }
 ]
 
