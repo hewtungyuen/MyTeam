94abe68cbef7f0457cfea87200a6b1fc3786d1b8
ziyuanqin <e0563845@u.nus.edu>
2022-03-15 16:55:35 +0800

    Sprint1: add two buttons

--- my-team/src/components/CreateProj.vue
+++ my-team/src/components/CreateProj.vue
@@ -1,30 +1,45 @@
 <template>
   <div id = "body">
+      <button id = "back" @click = "backHome()"> &#8249; Home </button>
     <h1 id = "heading"> Create a new project </h1>
     <div id = "proj_name">
         <form>
-        <label for="name" >Project Name: </label> <br><br>
-        <input type="text" id="name" required="" size = "50"> <br><br><br>
-        <label for="details">Project Details: </label> <br><br>
-        <textarea id="details" name="details" rows="4" cols="50"></textarea>
+        <label for="name" >Project Name: </label> <br>
+        <input type="text" id="name" required="" size = "30"> <br><br>
+        <label for="details">Project Details: </label> <br>
+        <textarea id="details" name="details" rows="4" cols="40"></textarea>
         </form>
     </div>
     <div id = "add_member">
         <form>
-            <label for="add" >Add New Members: </label> <br><br>
-            <input type="email" id="add" required="" size = "40">
-            <button id = 'add_new' round>
+            <label for="add" >Add New Members: </label> <br>
+            <input type="email" id="add" required="" placeholder="Enter Email" size = "20">
+            <button id = 'add_new' round @click = "addMember()">
                 +
-            </button> <br><br><br>
-            <label for="members">Team Members: </label> <br><br>
-            <textarea id="members" name="members" rows="4" cols="50"></textarea>
+            </button> <br><br>
+            <label for="members">Team Members: </label> <br>
+            <textarea id="members" name="members" rows="4" cols="40"></textarea>
         </form>
     </div>
+  <div>
+      <button id = "create" round @click = "createProj()"> Create &raquo; </button>
+  </div>
     </div>
 </template>
 
 <script>
 export default {
+    methods:{
+        backHome(){
+            this.$router.push('/HomePage');
+        },
+        addMember(){
+            alert("add member")
+        },
+        createProj(){
+            alert("create")
+        }
+    }
 
 }
 </script>
@@ -32,26 +47,35 @@ export default {
 <style>
 #body{
             position:fixed;
-            margin: 35px;
             font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
         }
         #heading{
-        margin-left: 550px
+        margin-left: 550px;
+        
+        
         /* Add a new project */
         }
+        
+        #back{
+            margin-left: 100px;
+            background-color: #04AA6D;
+            color: white;
+            padding: 5px;
+            font-size: 18px;
+            display: inline-block
+        }
         #proj_name{
-            margin-top: 50px;
-            margin-left: 60px;
+            margin-left: 140px;
             display:inline-block;
         }
         #add_member{
-            margin-top:50px;
-            margin-left: 60px;
+            margin-top:30px;
+            margin-left: 90px;
             display: inline-block;
             
         }
         form{
-            font-size: 24px;
+            font-size: 20px;
 
         }
         input, textarea{
@@ -59,6 +83,23 @@ export default {
             font-size: 20px;
             font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
         }
-
+        #add_new{
+            border-radius: 40%;
+            font-size: 25px;
+            position:relative;
+            margin-left: 10px;
+        }
+        #create{
+            background-color: #04AA6D;
+            color: white;
+            padding: 10px;
+            font-size: 20px;
+            margin-top: 50px;
+            margin-left: 1000px
+        }
+        #create:hover, #back:hover{
+            background-color: #ddd;
+            color: black;
+        }
 
 </style>
