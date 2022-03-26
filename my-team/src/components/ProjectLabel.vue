3fe03b3829ad9c30347f7b9f5b6e571b8cdadd48
Chang Yi Chen <changyichen@u.nus.edu>
2022-03-26 17:48:13 +0800

    Update ProjectLabel.vue

--- my-team/src/components/ProjectLabel.vue
+++ my-team/src/components/ProjectLabel.vue
@@ -16,6 +16,7 @@ import firebaseApp from '../firebase.js';
 import {getAuth, onAuthStateChanged} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 import {collection, getDocs} from "firebase/firestore";
+
 const db = getFirestore(firebaseApp);
 
 export default {
