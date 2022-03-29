ca5a9209cc7eb246b4bdeb2499436e2c68204150
hewtungyuen <hewtungyuen@gmail.com>
2022-03-29 11:38:38 +0800

    Sprint 2: dynamically render deadlines table with data from firebase

--- my-team/src/components/DeadlinesAndMeetings.vue
+++ my-team/src/components/DeadlinesAndMeetings.vue
@@ -3,28 +3,57 @@
   <n-card embedded :bordered="false">
     <h3 v-if = "type == 'Meeting'" @click = 'goToMeetings()'>{{title}}</h3>
     <h3 v-if = "type == 'Deadline'" @click = 'goToProjects()'>{{title}}</h3>
-    {{deadline}} 
+    Project: {{this.name}}
+    <br>
+    Deadline: {{deadline}} 
+    <br>
   </n-card>
 </div>
 
 </template>
 
 <script>
+import firebaseApp from '../firebase.js'
+import { collection, getDocs, getFirestore} from 'firebase/firestore'
+var db = getFirestore(firebaseApp)
+
 // need to calculate how many days left to the meeting 
 export default {
+    mounted(){
+        this.getProjectName()
+    },
+    data(){
+        return {
+            name: ''
+        }
+    },
     methods:{
         goToProjects(){
-            alert('go to project page')
-            // this.$router.push('ProjectPage')
+            this.$store.commit('update', this.projectId);
+            // console.log(projectIds)
+            this.$router.push('/ProjectPage/' + this.projectId);
         },
         goToMeetings(){
+
             alert('go to meetings tab in project page')
+        },
+        getProjectName(){
+            var allProjects = getDocs(collection(db,'Projects'))
+            allProjects.then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    if (doc.id == this.projectId) {
+                        this.name = doc.data().Name
+                    }
+                })
+            })
         }
+
     },
     props:{
         title:String,
         deadline:String,
         type:String,
+        projectId:String,
     }
 }
 
